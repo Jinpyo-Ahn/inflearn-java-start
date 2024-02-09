49441aceed80327c4f823398e0750e69b3fd3d44
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2024-02-09 19:44:12 +0900

    Chapter02 Variable

---
+++ src/variable/Var7.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var7 {
+
+    public static void main(String[] args) {
+        int a = 100; // 정수
+        double b = 1.33; // 실수
+        boolean c = true;
+        char d = 'A';
+        String f = "Hello Java";
+
+        int z = 100;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+    }
+}
---
+++ src/variable/exercise/VarEx3.java
@@ -0,0 +1,12 @@
+package variable.exercise;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long bigNumber = 10000000000L;
+        System.out.println(bigNumber);
+
+        boolean jin = true;
+        System.out.println("jin = " + jin);
+    }
+}
