f9715bb36ce8295212d4a1297622beb8030a521d
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2024-02-11 19:54:00 +0900

    Chapter05 loop

---
+++ src/loop/exercise/ForEx3.java
@@ -0,0 +1,12 @@
+package loop.exercise;
+
+public class ForEx3 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1, max = 5; i <= max; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
